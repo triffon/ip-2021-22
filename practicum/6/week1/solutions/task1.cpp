2b531c63adba98a65a8c5331d2418689c9a9ebba
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2021-10-08 23:24:06 +0300

    Create task1.cpp

---
+++ practicum/6/week1/solutions/task1.cpp
@@ -0,0 +1,11 @@
+//Задача първа
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0; //Декларира променлива от цял тип
+	cin >> a; //Въвежда стойността й с функцията cin (console in)
+	cout << (bool)(a); //Каства променливата към тип bool и я отпечатва 
+	//на конзолата, с функцията cout (console out)
+}
