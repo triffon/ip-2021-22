5c7f490f7a8703b57ecacbc7bc4cb967a33db995
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-10-11 12:24:45 +0300

    Първи две програми на C++

---
+++ lectures/1/hello/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 5;
+  cout << "a = " << a << endl;
+  cout << "2a = " << 2 * a << endl;
+  return 0;
+}
---
+++ lectures/1/second/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b;
+  // първо въвеждаме стойности
+  cout << "a = "; cin >> a;
+  cout << "b = "; cin >> b;
+
+  // събираме числата
+  int c = a + b;
+
+
+  // извеждаме резултата
+  cout << "a + b = " << c << endl;
+  return 0;
+}
