b890a5ed96f63356ee8cf87c172666823d7c6f6b
dafa <dafy.zoteva@gmail.com>
2021-10-17 12:51:27 +0300

    Add tasks and solutions for Week 02
    Data types. Operators.

---
+++ exercises/4/02 Types/Solutions/01_ConstructANumber.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+	// деклариране на променлива, в която ще бъде съхранена цифрата на стотиците
+	int hundreds;
+	std::cout << "Hundreds: ";
+	std::cin >> hundreds;
+
+	// променлива, в която ще бъде съхранена цифрата на десетиците
+	int tens;
+	std::cout << "Tens: ";
+	std::cin >> tens;
+
+	// променлива, в която ще бъде съхранена цифрата на единиците
+	int ones;
+	std::cout << "Ones: ";
+	std::cin >> ones;
+
+	int number = hundreds * 100 + tens * 10 + ones;
+	std::cout << "number = " << number << '\n';
+
+	return 0;
+}
