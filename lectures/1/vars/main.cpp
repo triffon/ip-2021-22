c45b42398b8a1ce0f5d2f47f249e8ea9c677e876
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-10-18 11:21:48 +0300

    Коректно сравнение на дробни числа

--- lectures/1/vars/main.cpp
+++ lectures/1/vars/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(int, char**) {
-    const float STEP =  0.25; // 0.31;
+    const float STEP = 0.31; // 0.25; // 0.31;
+    const float EPS = 1e-6; // 0.000001
     float x = STEP;
     x = x + STEP;
     x = x + STEP;
     x = x + STEP;
     x = x + STEP;
-    if (x == 1.25) {
+    if (fabs(x - 1.55) < EPS) {
         cout << "Да!" << endl;
     } else {
         cout << sizeof(x) << endl;
