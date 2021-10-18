b3e0a158dc8c5f4ccf2a4ee7c9fd9167df636b9d
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-10-18 11:41:44 +0300

    Примери за работа с типове

--- lectures/1/vars/main.cpp
+++ lectures/1/vars/main.cpp
@@ -17,4 +17,24 @@ int main(int, char**) {
         cout.precision(30);
         cout << "Не! x = " << x << endl;
     }
+
+    int y = 5;
+    cout << x + y << endl;
+    char c = 'C';
+    int code = c;
+    cout << "Кодът на " << c << " е " << code << endl;
+    y = x;
+    cout << "y = " << y << endl;
+    cout << 5 / 2 << endl;
+    cout << 5. / 2 << endl;
+    cout << 5e0 / 2 << endl;
+    cout << (float)5 / 2 << endl;
+    y = -2;
+    unsigned z = y;
+    cout << z << endl;
+    cout << (2 * 3 + 5) << ' ' << 5 + 2 * 3 << endl;
+    // !!! (cout << 2) < 3;
+    cout << (2 < 3) << endl;
+    // !!! (cout << (2 < 3)() && ((3 < 4) << endl);
+    cout << (2 < 3 && 3 < 4) << endl;
 }
