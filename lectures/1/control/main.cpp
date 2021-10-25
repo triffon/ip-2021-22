029ef9a204ee9cce41e7364ce69b6488a49310ac
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-10-25 10:57:58 +0300

    Проверка за растяща редица

--- lectures/1/control/main.cpp
+++ lectures/1/control/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-int main(int, char**) {
+int main1() {
     cout << "Hello, world!\n" << "It's me!" << endl;
     2 + 3;
     exp(sin(sqrt(15)));
@@ -26,4 +26,23 @@ int main(int, char**) {
         cout << x << endl;
     };;;;;
     cout << ((((((x)))))) << endl;
+    return 0;
+}
+
+int main() {
+    int a, b, c;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+
+    // !!! if ((c > b) > a)
+    /*
+    if (c > b && b > a) // || a > b && b > c)
+       cout << "Образуват растяща редица" << endl;
+    else
+       cout << "НЕ образуват растяща редица" << endl;
+    */
+    //c > b && b > a ? cout << "Образуват растяща редица" << endl : cout << "НЕ образуват растяща редица" << endl;
+    cout << (c > b && b > a ? "О" :"НЕ о") << "бразуват растяща редица" << endl;
+    return 0;
 }
