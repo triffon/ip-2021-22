136e856f0b361bb14a45831f204f4aae98e15ee3
dafa <dafy.zoteva@gmail.com>
2021-10-30 19:35:59 +0300

    Add tasks and solutions for Week 04
    Loops.

---
+++ exercises/4/04 Loops/Solutions/09_Digits.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+	unsigned number;
+	std::cout << "number = ";
+	std::cin >> number;
+
+	unsigned originalNumber = number;
+
+	unsigned maxDigit = 0;
+	unsigned countDigits = 0;
+
+	if (number == 0)
+	{
+		countDigits = 1;
+	}
+	else
+	{
+		while (number > 0)
+		{
+			unsigned currentDigit = number % 10;
+			if (maxDigit < currentDigit)
+			{
+				maxDigit = currentDigit;
+			}
+
+			++countDigits;
+
+			number = number / 10;
+			//number /= 10;
+		}
+	}
+	
+	std::cout << "The number of digits in " << originalNumber << " is " << countDigits << '\n';
+	std::cout << "The max digit in " << originalNumber << " is " << maxDigit << '\n';
+
+	return 0;
+}
