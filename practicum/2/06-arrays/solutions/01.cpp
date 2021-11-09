1cffe2982cf7c7c3ea88b6e0eef27bf8d81ea9c2
Valentin Dimitrov <91693492+Dimitroffline@users.noreply.github.com>
2021-11-10 00:13:12 +0200

    Add files via upload

---
+++ practicum/2/06-arrays/solutions/01.cpp
@@ -0,0 +1,27 @@
+/// Прочетете от конзолата цяло число numCount ∈ [1, 127] и след това N на брой цели числа.
+/// Създайте масив с елементи прочетените числа. Изведете на конзолата елементите на масива.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int MAX = 128;
+	int arr[MAX];
+	int numCount;
+
+	cin >> numCount;
+
+	for(int i = 0; i < numCount; ++i)
+    {
+        cin >> arr[i];
+    }
+
+	for(int i = 0; i < numCount; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+
+	return 0;
+}
