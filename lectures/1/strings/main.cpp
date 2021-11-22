e8dbbab549e63b8f749433fc29030d39afd59bf7
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-11-22 10:37:19 +0200

    Пример за buffer overflow

---
+++ lectures/1/strings/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void overflowExample() {
+    char s2[] = "sadjalkdjaskldjaskldjaskdjk";
+    char word[8] = { 'H', 'e', 'l', 'l', 'o', ' ', 'H', 'i'};
+    char s[] = "sdkjsalkdjaskldjaskldjaksdjkaldjkad";
+    cout << word << endl;
+
+}
+
+int main(int, char**) {
+    
+}
