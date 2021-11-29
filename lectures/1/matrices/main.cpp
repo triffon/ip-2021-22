d144c9eba9f8eac72cc18c9978c1a98be2bffd08
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-11-29 11:43:27 +0200

    Варианти на решение на транспониране на матрица

--- lectures/1/matrices/main.cpp
+++ lectures/1/matrices/main.cpp
@@ -16,11 +16,65 @@ void matrixExamples() {
             cin >> a[i][j];    // въведи елемента на индекс j в ред i
         }
     
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++)
+    for(int i = 0; i < m; i++) {      // за всеки ред i,
+        for(int j = 0; j < n; j++)    // изведи ред i, т.е. за всеки елемент j от него
+            cout << a[i][j] << '\t';  // го изведи
+        cout << endl;                 // и накрая сложи нов ред
+    }
+
+    /* Транспонирано извеждане, т.е. по колони вместо по редове */
+    /*
+    for(int j = 0; j < n; j++) {     // за всяка колона j
+        for(int i = 0; i < m; i++)   // изведи колона j на един ред на екрана, т.е. за всеки ред i
+            cout << a[i][j] << '\t'; // изведи елемента на ред i и колона j
+        cout << endl;                // и накрая сложи нов ред
+    }
+    */
+
+   // транспониране в нова матрица
+   /*
+   int b[MAX][MAX];
+   for(int j = 0; j < n; j++)   
+        for(int i = 0; i < m; i++)
+            b[j][i] = a[i][j];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++)
+            cout << b[i][j] << '\t';
+        cout << endl;
+    }
+    */
+
+   // транспониране на място по колони
+   /*
+   for(int j = 1; j < n; j++)   
+        for(int i = 0; i < j; i++) {
+            // a[j][i] = a[i][j]; // губим елементи!!!
+            int tmp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = tmp;
+        }
+    */
+
+   // транспониране на място по редове
+   for(int i = 0; i < m; i++)   
+        for(int j = i + 1; j < n; j++) {
+            // a[j][i] = a[i][j]; // губим елементи!!!
+            int tmp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = tmp;
+        }
+
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++)
             cout << a[i][j] << '\t';
         cout << endl;
     }
+
+    int columnSums[MAX] = { 0 };
+    
+
 }
 
 int main(int, char**) {
