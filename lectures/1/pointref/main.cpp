5bb3175a6ff9b048bf8bc47289f991fa15fbee83
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-12-13 12:05:52 +0200

    Примери с указатели, масиви и низове

---
+++ lectures/1/pointref/main.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+void testUndefinedPointer() {
+    // !!!!!!!
+    int* yolo;
+    cout << yolo << endl;
+    cout << *yolo << endl;
+    *yolo = 5;
+    cout << *yolo << endl;
+}
+
+void testPointers() {
+    // !!!
+    double d = 1.23;
+    double *pd = nullptr;
+    if (pd != nullptr)
+        cout << *pd << endl;
+    pd = &d;
+    if (pd != nullptr)
+        cout << *pd << endl;
+    double *qd = pd;
+    cout << d << ' ' << *pd << ' ' << *qd << ' ' << &d << ' ' << pd << ' ' << qd << endl;
+    // !!! qd = &4.56;
+    // *p = "вземи lvalue-то, което се сочи от p"
+    // &x = "вземи адреса на lvalue-то x" = "указател към lvalue-то x"
+    *qd = 4.56;
+    cout << d << ' ' << *pd << ' ' << *qd << ' ' << &d << ' ' << pd << ' ' << qd << endl;
+    d = 7.89;
+    cout << d << ' ' << *pd << ' ' << *qd << ' ' << &d << ' ' << pd << ' ' << qd << endl;
+    *pd = 1.23;
+    cout << d << ' ' << *pd << ' ' << *qd << ' ' << &d << ' ' << pd << ' ' << qd << endl;
+
+    double **    qqd = &qd;
+    // !!! **qqd = &qd;
+    // qqd == &qd
+    // *qqd == *&qd == qd == &d
+    // **qqd == **&qd == *qd == *&d == d
+    // !!! qqd = &&d;
+    cout << qqd << ' ' << &qd << ' ' << &pd << ' ' << *qqd << ' ' << **qqd << endl;
+    *pd = 3.45;
+    cout << qqd << ' ' << &qd << ' ' << &pd << ' ' << *qqd << ' ' << **qqd << endl;
+    **qqd = 3.15;
+    cout << d << ' ' << *pd << ' ' << *qd << ' ' << &d << ' ' << pd << ' ' << qd << ' ' << *qqd << ' ' << **qqd << endl;
+    pd = qd;
+    // !!! &*pd = qd; // !!! &d = qd; 
+    pd = &*qd; // <-> pd = qd; <-> pd == &d;
+}
+
+void printArrayWithPointers(int a[], int n) {
+    cout << sizeof(a) << ' ' << sizeof(a[0]) << endl; // !!!
+    for(int i = 0; i < n; i++)
+        cout << "a[" << i << "]: " << &a[i] << ' ' << a[i] << endl;
+}
+
+void testPointersAndArrays() {
+    const int MAX = 10;
+    int a[MAX];
+    for(int i = 0; i < MAX; i++)
+        a[i] = i + 1;
+    cout << a << ' ' << &a[0] << endl;
+    printArrayWithPointers(a, MAX);
+    int* p = a;
+    cout << p << ' ' << *p << endl;
+    *p = 15;
+    printArrayWithPointers(a, MAX);
+    *a = 20;
+    printArrayWithPointers(a, MAX);
+    p = &a[1];
+    *p = 30;
+    printArrayWithPointers(a, MAX);
+    // !!! a = &a[1];
+    p = a;
+    p = 4 + p;
+    cout << a << ' ' << p << endl;
+    cout << *a << ' ' << *p << endl;
+    *p = 40; // <-> *(a + 4) <-> *(4 + a) <-> a[4]
+    printArrayWithPointers(a, MAX);
+    cout << sizeof(int) << endl;
+    cout << sizeof(5) << endl;
+    cout << sizeof(a[1]) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(p) << endl;
+    cout << sizeof(*p) << endl;
+    cout << sizeof(a) << endl;
+    cout << sizeof(a) / sizeof(a[0]) << endl; // !!!
+    cout << sizeof(long) << endl;
+    long address = (long)p;
+    cout << p << ' ' << address << endl;
+    cout << (long)(p + 2) << ' ' << address + 2 * sizeof(int) << endl;
+    /*
+    !!!!!!
+    address = 98437598432;
+    p = (int*)address;
+    cout << *p << endl;
+    !!!!!!
+    */
+    cout << *p-- << endl; // *p-- <-> *(p--)
+    cout << *p << endl;
+    cout << *--p << endl;
+    cout << *p << endl;
+    (*(a+4))--;
+    printArrayWithPointers(a, MAX);
+    cout << 7[a] << endl; // <-> a[7]
+}
+
+void testPointersAndStrings() {
+    char s[] = "Hello, world!";
+    char* p = s;
+    cout << p << endl;
+    while (*p)
+        cout << *p++;
+    cout << endl;
+    cout << s << endl;
+    cout << p << endl;
+    p = s;
+    while (*p)
+        cout << p++ << endl;
+
+    int a[] = { 1, 2, 3, 4, 5, 0 };
+    int* q = a;
+    cout << a << endl;
+    cout << q << endl;
+    while (*q)
+        cout << q++ << endl;
+
+    int len = 0;
+    p = s;
+    // !!!while (*s++)
+    while (*p++)
+        // p е len+1 стъпки от началото на s
+        len++;
+    cout << len << endl;
+}
+
+int main() {
+    // testUndefinedPointer();
+    // testPointers();
+    // testPointersAndArrays();
+    testPointersAndStrings();
+}
