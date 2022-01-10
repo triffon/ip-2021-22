95772e5e7facb90d38b108da099f0b480b541bd3
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-01-10 10:47:43 +0200

    Довършване на задачата за транспониране на матрица в динамичната памет

--- lectures/1/dynamic/main.cpp
+++ lectures/1/dynamic/main.cpp
@@ -19,16 +19,24 @@ int* readArray(unsigned n) {
     return a;
 }
 
-int** readDynamicMatrix(unsigned m, unsigned n) {
+int** createDynamicMatrix(unsigned m, unsigned n) {
     int** matrix = new int*[m];
-    for(int i = 0; i < m; i++) {
+    for(int i = 0; i < m; i++)
         matrix[i] = new int[n];
+    return matrix;
+}
+
+int** readDynamicMatrix(int** matrix, unsigned m, unsigned n) {
+    for(int i = 0; i < m; i++) 
         for(int j = 0; j < n; j++) {
             cout << "a[" << i << "][" << j << "] = ";
             cin >> matrix[i][j]; // <-> *(*(matrix + i) + j)
         }
-    }
     return matrix;
+}
+
+int** readDynamicMatrix(unsigned m, unsigned n) {
+    return readDynamicMatrix(createDynamicMatrix(m, n), m, n);
 }
 
 void printDynamicMatrix(int **a, unsigned m, unsigned n) {
@@ -37,6 +45,12 @@ void printDynamicMatrix(int **a, unsigned m, unsigned n) {
             cout << a[i][j] << '\t';
         cout << endl;
     }
+}
+
+void deleteDynamicMatrix(int **a, unsigned m) {
+    for(int i = 0; i < m; i++)
+        delete[] a[i];
+    delete[] a;
 }
 
 double findAverage(int a[], unsigned n) {
@@ -56,10 +70,33 @@ void calculateAverage() {
 void testMatrix() {
     int** matrix = readDynamicMatrix(2, 3);
     printDynamicMatrix(matrix, 2, 3);
+    deleteDynamicMatrix(matrix, 2);
+}
+
+int** transposeMatrix(int **matrix, unsigned m, unsigned n) {
+    int ** newmatrix = createDynamicMatrix(n, m);
+    for(int j = 0; j < n; j++)   
+        for(int i = 0; i < m; i++)
+            newmatrix[j][i] = matrix[i][j];
+    // изтриваме старата матрица
+    deleteDynamicMatrix(matrix, m);
+    return newmatrix;
+}
+
+void testTranspose() {
+    unsigned m, n;
+    cout << "m = ";cin >> m;
+    cout << "n = ";cin >> n;
+    int ** matrix = readDynamicMatrix(m, n);
+    printDynamicMatrix(matrix, m, n);
+    matrix = transposeMatrix(matrix, m, n);
+    printDynamicMatrix(matrix, n, m);
+    deleteDynamicMatrix(matrix, n);
 }
 
 int main(int, char**) {
     //for(int i = 0; i < 100000; i++)
         //calculateAverage();
-    testMatrix();
+    // testMatrix();
+    testTranspose();
 }
