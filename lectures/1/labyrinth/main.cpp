fea79dce636c0cd33de08c6d1bd29f906566cefe
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-01-17 12:47:45 +0200

    Намерихме къде е съкровището

--- lectures/1/labyrinth/main.cpp
+++ lectures/1/labyrinth/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const unsigned MAX = 10;
 
 
-bool findTreasure(char labyrinth[][MAX], unsigned n, int x, int y) {
+bool findTreasure(char labyrinth[][MAX], unsigned n, int x, int y, int& tx, int& ty) {
     cout << "Пробваме да стъпим на (" << x << ',' << y << ")" << endl;
     // ако съм излязъл извън границите
     // или
@@ -19,21 +19,23 @@ bool findTreasure(char labyrinth[][MAX], unsigned n, int x, int y) {
     if (labyrinth[x][y] == '$') {
         // успех!
         cout << "Съкровище!" << endl;
+        tx = x;
+        ty = y;
         return true;
     }
     // маркирам, че съм стъпил на [x][y]
     labyrinth[x][y] = '.';
     // стъпка на север
-    if (findTreasure(labyrinth, n, x - 1, y   ))
+    if (findTreasure(labyrinth, n, x - 1, y   , tx, ty))
         return true;
     // стъпка на юг
-    if (findTreasure(labyrinth, n, x + 1, y   ))
+    if (findTreasure(labyrinth, n, x + 1, y   , tx, ty))
         return true;
     // стъпка на изток
-    if (findTreasure(labyrinth, n, x    , y + 1))
+    if (findTreasure(labyrinth, n, x    , y + 1, tx, ty))
         return true;
     // стъпка на запад
-    if (findTreasure(labyrinth, n, x    , y - 1))
+    if (findTreasure(labyrinth, n, x    , y - 1, tx, ty))
         return true;
     // връщане назад
     cout << "Връщаме се назад от (" << x << ',' << y << ")" << endl;
@@ -50,8 +52,9 @@ void testLabyrinth() {
             "  ** "
         };
     int startx = 4, starty = 1;
-    if (findTreasure(labyrinth, 5, startx, starty))
-        cout << "Намерихме съкровището!" << endl;
+    int tx = -1, ty = -1;
+    if (findTreasure(labyrinth, 5, startx, starty, tx, ty))
+        cout << "Намерихме съкровището на позиция (" << tx << ',' << ty << ")!" << endl;
     else
         cout << "Не успяхме да намерим съкровището..." << endl;
 }
